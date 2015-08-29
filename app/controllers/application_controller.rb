defd70b16b2c7a529f5762df7a5b5c628e81e2e6
Dan Segal <danSegal2@gmail.com>
2015-08-29 01:01:50 -0400

    Add some debugging tools (greyed boxes and errors)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def _raise(message)
+    if Rails.env.development?
+      binding.pry
+    else
+      raise message
+    end
+  end
+
 end
